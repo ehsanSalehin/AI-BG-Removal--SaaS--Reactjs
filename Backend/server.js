55bf1efe6adfa87d9bbab881e878faace875c55b
ehsan <eh.salehin@gmail.com>
2024-11-17 14:25:07 -0500

    new update v1.1.4

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 import connectDB from './configs/mongodb.js'
+import userRouter from './routes/userRoute.js'
 
 //config
 const PORT = process.env.PORT || 4000
@@ -14,6 +15,7 @@ app.use(cors())
 
 //API 
 app.get('/',(req, res)=>res.send("it is working"))
+app.use('/api/user', userRouter)
 
 
  
