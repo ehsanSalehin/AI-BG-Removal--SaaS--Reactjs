93454001cc101564de12e0c0bcd8d95a7292667a
ehsan <eh.salehin@gmail.com>
2024-11-18 05:58:41 -0500

    new update v1.1.6

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 const userSchema = new mongoose.Schema({
     clerkId:{
         type:String,
-        required:true,
+        required:false,
         unique:true
     },
     email:{
